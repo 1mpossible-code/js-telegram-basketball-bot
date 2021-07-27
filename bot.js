7f20f962f207739635af72b8007832a427a721aa
Max <linme00p@gmail.com>
2021-07-27 22:34:38 +0300

    Create base bot.js structure. Configure 'dotenv' and create '/start' command handler

---
+++ bot.js
@@ -0,0 +1,18 @@
+import {Telegraf} from 'telegraf'
+import dotenv from "dotenv";
+
+// Configure 'dotenv'
+dotenv.config();
+
+// Create bot instance
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+// Start command handler
+bot.start(ctx => ctx.reply('Hello!'));
+
+// Launch bot
+bot.launch().then(() => console.log('Bot is ONLINE'));
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
