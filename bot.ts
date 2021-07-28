216bc43e584516b1dd6b2db53ed83c789f63bce6
Max <linme00p@gmail.com>
2021-07-28 19:55:35 +0300

    Add basic basketball dice handler. If player send emoji with basketball, the bot checks the result of the dice and sends 'win' or 'lose' message

--- bot.ts
+++ bot.ts
@@ -10,6 +10,27 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 // Start command handler
 bot.start(ctx => ctx.reply('Hello!'));
 
+// Basketball dice handler
+bot.on('dice', (ctx) => {
+    // If dice emoji is basketball
+    if (ctx.message.dice.emoji === '🏀') {
+        // Value '5' is the winning value, so everything
+        // else is losing values
+        //
+        // Set timeout to send message about the
+        // dice status after the animation
+        if (ctx.message.dice.value === 5) {
+            setTimeout(
+                () => ctx.reply('You win'), 2500
+            )
+        } else {
+            setTimeout(
+                () => ctx.reply('You lose'), 2500
+            )
+        }
+    }
+});
+
 // Launch bot
 bot.launch().then(() => console.log('Bot is ONLINE'));
 
