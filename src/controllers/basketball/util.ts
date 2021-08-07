708f79d349632aca317d02a517d32f640a405452
Max <linme00p@gmail.com>
2021-08-07 20:35:30 +0300

    Add create user function to basketball controller util

--- src/controllers/basketball/util.ts
+++ src/controllers/basketball/util.ts
@@ -1,11 +1,29 @@
 import MyContext from "../../types/IMyContext";
+import Player, {IPlayer} from "../../models/Player";
+
+export interface IUser {
+    _id: string,
+    name: string
+}
 
 /**
  * Delete message aster 5000 ms.
  * @param ctx
  */
 export const deleteMessage = (ctx: MyContext) => {
-    setTimeout(() => {
-        ctx.deleteMessage();
+    setTimeout(async () => {
+        await ctx.deleteMessage();
     }, 5000);
+}
+
+/**
+ * Create or find player from db
+ * @param user
+ */
+export const createPlayer = async (user: IUser) => {
+    return await Player.findOne({_id: user._id}) ?? await Player.create(<IPlayer>{
+        _id: user._id,
+        name: user.name,
+        score: 0,
+    })
 }
