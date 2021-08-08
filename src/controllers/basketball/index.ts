cb1110580cc39e3f6b641f35ba76a5b6aa81122a
Max <linme00p@gmail.com>
2021-08-08 13:46:20 +0300

    Create base logic for entering basketball scene. Replace console logging with 'winston' logger. Update dice function

--- src/controllers/basketball/index.ts
+++ src/controllers/basketball/index.ts
@@ -1,6 +1,7 @@
-import {Scenes, Markup} from 'telegraf';
+import {Markup, Scenes} from 'telegraf';
 import MyContext from "../../types/IMyContext";
-import {deleteMessage} from "./util";
+import {deleteMessage, getPlayer, getRoom, IChat, IUser, timeoutMessage} from "./util";
+import logger from "../../util/logger";
 
 // Inline keyboard markup, that shows
 // itself when enter 'basketball' scene
@@ -9,40 +10,57 @@ const enterReplyOptions = Markup.inlineKeyboard([
     Markup.button.callback('Start', 'start'),
 ])
 
-// Handler factories
-const {leave} = Scenes.Stage;
 // Echo scene
 const basketballScene = new Scenes.BaseScene<MyContext>('basketball');
 
 // Enter message
-basketballScene.enter((ctx) => ctx.reply('Basketball scene greeting', enterReplyOptions));
+basketballScene.enter(async (ctx: MyContext) => {
+    await ctx.reply('Basketball scene greeting', enterReplyOptions);
+
+    const user: IUser = {
+        _id: String(ctx.from?.id),
+        name: String(ctx.from?.first_name),
+    };
+
+    const owner = await getPlayer(user);
+
+    logger.debug(owner);
+
+    const chat: IChat = {
+        _id: String(ctx.chat?.id),
+        owner: owner,
+    };
+
+    const room = await getRoom(chat);
+
+    logger.debug(room);
+});
 // Leave message
 basketballScene.leave((ctx) => ctx.reply('Basketball scene leave'));
 // Handle exit command
-basketballScene.command('exit', leave<MyContext>());
+basketballScene.command('exit', Scenes.Stage.leave<MyContext>());
 // Dice handler
 basketballScene.on('dice', (ctx) => {
     // If dice emoji is basketball
     if (ctx.message.dice.emoji === '🏀') {
+        // Extract value from dice result
+        const {value} = ctx.message.dice;
+        logger.debug(`The dice rolled up with value: ${value}`);
         // Value '5' is the winning value, so everything
         // else is losing values
         //
         // Set timeout to send message about the
         // dice status after the animation
-        if (ctx.message.dice.value === 5) {
-            setTimeout(
-                () => ctx.reply('You win'), 2500
-            )
+        if (value === 5 || value === 4) {
+            timeoutMessage(ctx, 'You win', 4000);
         } else {
-            setTimeout(
-                () => ctx.reply('You lose'), 2500
-            )
+            timeoutMessage(ctx, 'You lose', 4000);
         }
     }
 });
 basketballScene.on("callback_query", (ctx) => {
-    console.log(ctx);
-    console.log(ctx.callbackQuery.from.id);
+    logger.info(ctx);
+    logger.info(ctx.callbackQuery.from.id);
 })
 // Delete all other messages
 basketballScene.on('message', deleteMessage);
